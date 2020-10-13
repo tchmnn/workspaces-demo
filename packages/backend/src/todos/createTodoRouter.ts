a298bc5b9dd03de71d6dadc27118d390f9eadc17
tchmnn <44507274+tchmnn@users.noreply.github.com>
2020-10-13 21:52:45 +0200

    refactor: use ajv in post handler aswell

--- packages/backend/src/todos/createTodoRouter.ts
+++ packages/backend/src/todos/createTodoRouter.ts
@@ -5,7 +5,10 @@ import { Context, Next } from 'koa';
 import bodyParser from 'koa-bodyparser';
 import Ajv from 'ajv';
 import patchSchema from './schemas/patch-todo-schema.json';
+import postSchema from './schemas/post-todo-schema.json';
 import { Todo } from '@workspaces-demo/common';
+
+const ajv = new Ajv();
 
 const createGetHandler = (todoService: TodoService) =>
   (ctx: Context, _next: Next) => {
@@ -28,28 +31,23 @@ const createDetailGetHandler = (todoService: TodoService) =>
     ctx.body = todo;
   }
 
+const validatePost = ajv.compile(postSchema);
 const createPostHandler = (todoService: TodoService) =>
   (ctx: Context, _next: Next) => {
     const { body } = ctx.request;
-
-    if (!body.title) {
-      ctx.status = 400;
-      ctx.body = "Field `title` is required"
-      return;
-    }
-
-    if (!body.content) {
-      ctx.status = 400;
-      ctx.body = "Field `content` is required"
-      return;
-    }
+    const valid = validatePost(body);
     
+    if (!valid && validatePost.errors) {
+      ctx.status = 400;
+      ctx.body = { errors: validatePost.errors.map(e => e.message) };
+      return;
+    }
+
     const todo = todoService.createTodo(body.title, body.content);
     ctx.status = 201;
     ctx.body = todo;
   }
 
-const ajv = new Ajv();
 const validatePatch = ajv.compile(patchSchema);
 const createDetailPatchHandler = (todoService: TodoService) =>
   (ctx: Context) => {
