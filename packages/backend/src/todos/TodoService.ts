106c07614d6346a5e53ce82f1bbef99b4ac173e7
tchmnn <44507274+tchmnn@users.noreply.github.com>
2020-10-13 20:17:36 +0200

    feat: add updateTodo

--- packages/backend/src/todos/TodoService.ts
+++ packages/backend/src/todos/TodoService.ts
@@ -31,6 +31,23 @@ class TodoService {
   getAllTodos (): Todo[] {
     return Object.values(this.todos);
   }
+
+  updateTodo (id: string, update: Partial<Todo>): Todo {
+    const todo = this.getTodo(id);
+    const updated = {
+      ...todo,
+      ...update,
+    }
+
+    if (update.id) {
+      this.deleteTodo(id);
+      this.todos[updated.id] = updated;
+    } else {
+      this.todos[id] = updated;
+    }
+
+    return updated;
+  }
 }
 
 export default TodoService;
